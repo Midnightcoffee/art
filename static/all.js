225ab305afaecd6d6138bd1e1c7985fc20ab98b1
Drew Verlee <drew.verlee@atomicobject.com>
2014-12-01 13:03:51 -0500

    coming and going alerts

--- static/all.js
+++ static/all.js
@@ -1,1 +1,25 @@
+var come_alert, coming, going, leave_alert;
 
+coming = function() {
+  return alert("Please buy something");
+};
+
+going = function() {
+  return alert("Go back and buy more art!");
+};
+
+leave_alert = function() {
+  var uri;
+  uri = document.documentURI;
+  if (uri.search('purchase') !== -1) {
+    return going();
+  }
+};
+
+come_alert = function() {
+  var uri;
+  uri = document.documentURI;
+  if (uri.search('purchase') !== -1) {
+    return coming();
+  }
+};
--- static/js/test.js.coffee
+++ static/js/test.js.coffee
@@ -1,3 +1,18 @@
-#alert("stuff = #{5+3}")
+coming =() ->
+  alert("Please buy something")
+
+going =() ->
+  alert("Go back and buy more art!")
+
+leave_alert =() ->
+  uri = document.documentURI
+  if uri.search('purchase') != -1 then going()
+
+come_alert =() ->
+  uri = document.documentURI
+  if uri.search('purchase') != -1 then coming()
+
+
+
 
 
